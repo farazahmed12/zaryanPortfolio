62200f7a5b61701ee53fca34624b5d592c5cdf2f
Faraz Ahmed <masterdot707@gmail.com>
2024-06-18 10:35:30 +0500

    removed file

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 "use client";
 import Image from "next/image";
-import AnimatedComponent from "./components/Animated";
 import HeroSection from "./components/HeroSection";
 import SecondSection from "./components/SecondSection";
 import ThirdSection from "./components/ThirdSection";
